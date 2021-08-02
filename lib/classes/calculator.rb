13b67b8aada8b6232621347a716df63740100f89
Iohara Pereira <iohara.pereira@m4u.com.br>
2021-08-01 23:55:53 -0300

    Classes modeladas e testadas, métodos calculator iniciados

--- lib/classes/calculator.rb
+++ lib/classes/calculator.rb
@@ -8,3 +8,22 @@ class Calculator
         total_payment()
         calculate_result()
     end
+
+    def total_order()
+        count = 0
+    # .each, while ou if?
+
+        @price_total = count
+    end
+
+    def total_payment()
+        count = 0
+    # .each, while ou if?
+        
+        @current_payment = count 
+    end
+    
+    def calculate_result
+        @result = @price_total - @current_payment
+    end
+        
--- lib/coffee_app.rb
+++ lib/coffee_app.rb
@@ -1,13 +1,32 @@
 # Put your code here!
 require 'classes/drink'
+require 'classes/order'
+require 'classes/payment'
 class CoffeeApp
   def self.call(prices_json, orders_json, payments_json)
 
-    drink_name = JSON.parse(prices_json)
-    name = drink_name[0]["drink_name"]
-    price = drink_name[0]["prices"]
-    drink_list = Drink.new(name, price)
+    drinks_list = JSON.parse(prices_json)
+    drink_name = drinks_list[0]["drink_name"]
+    drink_price = drinks_list[0]["prices"]
+
+    drink_list = Drink.new(drink_name, drink_price)
     puts drink_list
+
+    orders_list = JSON.parse(orders_json)
+    user_name = orders_list[0]["user"]
+    drink_name = orders_list[0]["drink"]
+    size_drink = orders_list[0]["size"]
+
+    order_list = Order.new(user_name, drink_name, size_drink)
+    puts order_list
+
+    payments_list = JSON.parse(payments_json)
+    client_name = payments_json[0]["user"]
+    amount_client = payments_json[0]["amount"]
+
+    payment_list = Payment.new(client_name, amount_client)
+    puts payment_list
+
   end
 end
 
