8fc132b4cf10db27a70af14d4e8e48c72d41adf0
Iohara Pereira <iohara.pereira@m4u.com.br>
2021-08-09 08:23:36 -0300

    Order builder

--- lib/builders/order_builder.rb
+++ lib/builders/order_builder.rb
@@ -5,4 +5,11 @@ class OrderListBuilder(orders_json, drink_list)
 
     JSON.parse(orders_json).each |order|
         drink_list.each do |drink|
-            if (order[drink] == drink.name) # order[drink] é comparável com drink.name?
+            if (order[drink] == drink.name) 
+                order_list.push(Order.new(order['user'], drink, order['size'])
+            end
+        end
+    end
+
+    order_list
+end
