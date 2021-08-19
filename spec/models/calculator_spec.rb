cde586c4ad071c308c7c1d4ae6ed0eb692831979
Iohara Pereira <iohara.pereira@m4u.com.br>
2021-08-19 15:28:06 -0300

    Cenários testes classe Calculator

--- spec/models/calculator_spec.rb
+++ spec/models/calculator_spec.rb
@@ -12,4 +12,25 @@ describe Calculator do
         
         expect(user_result.user_debt).to eq({"user"=>"coach", "order_total"=>8.00, "payment_total"=>2.50, "balance"=>5.50})
     end
+    it 'if the size of the drink does not exist' do
+        order1 = Order.new('coach', Drink.new('long black', {'small'=>3.25,'medium'=>3.50}), 'medium')
+        order2 = Order.new('coach', Drink.new("flat white", {'small'=>3.50, 'medium'=>4.00}), 'large')
+        payment = Payment.new("coach", 2.50)
+        user = User.new('coach')
+        user_result = Calculator.new(user)
+        user.set_payment_list([payment])
+        user.set_order_list([order1,order2])
+        
+        expect(user_result.user_debt).to eq({"user"=>"coach", "order_total"=>3.50, "payment_total"=>2.50, "balance"=>1.0})
+    end
+    it 'if the price of the drink is empty' do
+        order = Order.new('coach', Drink.new('long black', {'small'=>3.25,'medium'=>nil}), 'medium')
+        payment = Payment.new("coach", 2.50)
+        user = User.new('coach')
+        user_result = Calculator.new(user)
+        user.set_payment_list([payment])
+        user.set_order_list([order])
+        
+        expect(user_result.user_debt).to eq({"user"=>"coach", "order_total"=>0, "payment_total"=>2.50, "balance"=>-2.50})
+    end
 end
