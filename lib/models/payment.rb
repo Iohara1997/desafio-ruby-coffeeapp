70a7919bc9aa0f745e43519f6abca4131ab0351d
gitDbits <dbs.noronha@gmail.com>
2021-08-25 15:08:15 -0400

    Added method valid payment

--- lib/models/payment.rb
+++ lib/models/payment.rb
@@ -1,8 +1,12 @@
 class Payment
-    attr_reader :user, :account
+  attr_reader :user, :account
 
-    def initialize(user, account)
-        @user = user
-        @account = account
-    end
+  def initialize(user, account)
+    @user = user
+    @account = account
+  end
+
+  def self.valid_payment(payment)
+    !payment['user'].nil? and !payment['amount'].nil?
+  end
 end
