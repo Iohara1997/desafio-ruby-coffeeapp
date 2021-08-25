44ab5e0ace448261ba16fe63b4b1f5ffed3c9b4d
gitDbits <dbs.noronha@gmail.com>
2021-08-25 15:07:53 -0400

    Added method valid order

--- lib/models/order.rb
+++ lib/models/order.rb
@@ -1,9 +1,13 @@
-class Order 
-    attr_reader :user, :drink, :size
+class Order
+  attr_reader :user, :drink, :size
 
-    def initialize(user, drink, size)
-        @user = user
-        @drink = drink 
-        @size = size 
-    end 
+  def initialize(user, drink, size)
+    @user = user
+    @drink = drink
+    @size = size
+  end
+
+  def self.valid_order(order, drink)
+    !order['user'].nil? and !drink.nil? and !order['size'].nil?
+  end
 end
